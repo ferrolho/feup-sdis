35e62d84a494827af165c84d96a96b4b023b17de
Henrique Ferrolho <henriqueferrolho@gmail.com>
2015-02-25 11:10:24 +0000

    first commit

---
+++ src/l01/Main.java
@@ -0,0 +1,9 @@
+package l01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
---
+++ src/l01/Server.java
@@ -0,0 +1,11 @@
+package l01;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Server {
+
+	DatagramPacket datagramPacket;
+	DatagramSocket datagramSocket;
+	
+}
