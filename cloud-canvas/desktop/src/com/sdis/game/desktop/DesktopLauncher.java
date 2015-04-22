6c7cc82dc66a8cd46f46a2ad1550b179c7d98511
Tantch <ei12019@fe.up.pt>
2015-04-22 12:33:37 +0100

    projeto criado

---
+++ cloud-canvas/desktop/src/com/sdis/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sdis.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sdis.game.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
