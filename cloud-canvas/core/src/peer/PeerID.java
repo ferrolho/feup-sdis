3bda0380a98761cc88a0253bf7f66540828deb4a
ferrolho <henriqueferrolho@gmail.com>
2015-05-27 12:31:13 +0100

    working on tcp connections

---
+++ cloud-canvas/core/src/peer/PeerID.java
@@ -0,0 +1,52 @@
+package peer;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+
+public class PeerID implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private InetAddress ip;
+	private int port;
+
+	public PeerID(InetAddress ip, int port) {
+		this.ip = ip;
+		this.port = port;
+	}
+
+	public InetAddress getIP() {
+		return ip;
+	}
+
+	@Override
+	public String toString() {
+		return ip + ":" + port;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+
+		if (obj == null)
+			return false;
+
+		if (getClass() != obj.getClass())
+			return false;
+
+		PeerID other = (PeerID) obj;
+
+		if (ip == null) {
+			if (other.ip != null)
+				return false;
+		} else if (!ip.equals(other.ip))
+			return false;
+
+		if (port != other.port)
+			return false;
+
+		return true;
+	}
+
+}
